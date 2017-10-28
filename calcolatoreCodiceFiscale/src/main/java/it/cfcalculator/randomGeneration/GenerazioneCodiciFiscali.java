b598dac735cb7882ff995bc6061327b4d8b37ab4
csciandrone <carlo.sciandrone@gmail.com>
2017-10-28 22:47:24 +0200

    add comment

--- calcolatoreCodiceFiscale/src/main/java/it/cfcalculator/randomGeneration/GenerazioneCodiciFiscali.java
+++ calcolatoreCodiceFiscale/src/main/java/it/cfcalculator/randomGeneration/GenerazioneCodiciFiscali.java
@@ -4,7 +4,14 @@ import it.cfCalculator.CfValidationException;
 import it.cfCalculator.model.CfInputRequest;
 
 public interface GenerazioneCodiciFiscali {
-	
+	/**
+	 * <p>
+	 * Genera un numero arbitrario di codici fiscali
+	 * </p>
+	 * @param numero il numero di codici fiscali che si vuole generare
+	 * @return
+	 * @throws CfValidationException
+	 */
 	public Map<String, CfInputRequest> getRandomCf(int numero) throws CfValidationException;
 	
 	
